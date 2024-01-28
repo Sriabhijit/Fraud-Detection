d205799f875eb89a3717776c7efccd4621e5bd34
Sriabhijit <sri19310@ece.ssn.edu.in>
2024-01-28 11:29:56 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all the models related to ml here")
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+def model3():
+    print("adding code for model3")
